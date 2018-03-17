9f41b63c984a34e2f8ecce5f11961d1164bed7c9
Daniil Kostin <dd@dgr.ru>
2018-03-17 10:53:50 +0300

    client: only one axios global instance

--- client/src/plugins/axios.js
+++ client/src/plugins/axios.js
@@ -4,8 +4,6 @@ import Config from 'src/config'
 export default ({ Vue }) => {
   axios.defaults.baseURL = Config('api.api_url') // global config
 
-  // config for vue files, global not works
-  Vue.prototype.$axios = axios.create({
-    baseURL: Config('api.api_url')
-  })
+  // apply global instance
+  Vue.prototype.$axios = axios
 }
